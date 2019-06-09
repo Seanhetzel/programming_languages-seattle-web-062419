e1864f67891c1b718d0c14d34c379bb505e9ddfe
Sean Hetzel <seanhetzel1@gmail.com>
2019-06-09 04:32:54 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,7 +2,7 @@ def reformat_languages(languages)
   # your code here
   new_hash = {}
   languages.each do |oo_or_func, language|
-    new_hash[oo_or_func] = "test"
+    new_hash[language] = "test"
   end
   new_hash
 end
