dafd320fb0ac7d86995e7e531036edb70b6c12c0
Sean Hetzel <seanhetzel1@gmail.com>
2019-06-09 05:37:09 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -7,7 +7,7 @@ def reformat_languages(languages)
     language.each do |language_name, language_attributes|
       #if language_name == language
         
-      oo_or_func_array << oo_or_func[language_name]
+      oo_or_func_array << :language_name
       #end
       new_hash[language_name] = {:type => language_attributes.values.join, :style => [oo_or_func_array]}
     end
